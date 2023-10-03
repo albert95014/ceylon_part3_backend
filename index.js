58fd964e745da3111e9604c4df6d25ec3539bbfc
albert95014 <albertyang.me@gmail.com>
2023-10-02 19:33:40 -0700

    fix persons

--- index.js
+++ index.js
@@ -26,23 +26,23 @@ app.use(requestLogger)
 morgan.token('person', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
-// let persons = [
-//     { 
-//       "id": 1,
-//       "name": "Arto Hellas", 
-//       "number": "040-123456"
-//     },
-//     { 
-//       "id": 2,
-//       "name": "Ada Lovelace", 
-//       "number": "39-44-5323523"
-//     },
-//     { 
-//       "id": 3,
-//       "name": "Dan Abramov", 
-//       "number": "432432432432"
-//     }
-// ]
+let persons = [
+    // { 
+    //   "id": 1,
+    //   "name": "Arto Hellas", 
+    //   "number": "040-123456"
+    // },
+    // { 
+    //   "id": 2,
+    //   "name": "Ada Lovelace", 
+    //   "number": "39-44-5323523"
+    // },
+    // { 
+    //   "id": 3,
+    //   "name": "Dan Abramov", 
+    //   "number": "432432432432"
+    // }
+]
 
 // if (process.argv.length > 3) {
 //   const person = new Person({
@@ -76,10 +76,13 @@ app.get('/api/persons', (request, response) => {
   Person.find({}).then(result => {
     console.log("phonebook:")
     result.forEach(person => {
+        persons = persons.concat(person)
         console.log(`${person.name} ${person.number}`)
     })
-    // mongoose.connection.close()
-})
+    mongoose.connection.close()
+  })
+
+  response.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
